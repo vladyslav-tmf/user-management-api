7f94955ddccc1466014ce112a59a30f57ff2d9b3
Vladyslav Timofieiev <vladislav.tmf@gmail.com>
2025-03-15 09:18:29 +0200

    Update database configuration to use only PostgreSQL

--- app/config.py
+++ app/config.py
@@ -1,5 +1,16 @@
+import os
+
+
 class Config:
     """Base configuration class."""
 
-    SQLALCHEMY_DATABASE_URI: str = "sqlite:///users.db"
+    DB_USER: str = os.getenv("POSTGRES_USER")
+    DB_PASSWORD: str = os.getenv("POSTGRES_PASSWORD")
+    DB_HOST: str = os.getenv("POSTGRES_HOST")
+    DB_PORT: str = os.getenv("POSTGRES_PORT")
+    DB_NAME: str = os.getenv("POSTGRES_DB")
+
+    SQLALCHEMY_DATABASE_URI: str = (
+        f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS: bool = False
